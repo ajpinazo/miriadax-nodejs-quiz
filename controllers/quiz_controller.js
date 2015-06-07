0aa9c5cb23b7868f785aa82925ac6926d19d5f71
ajpinazo <ajpinazo@gmail.com>
2015-06-07 17:24:42 +0200

    Modelo bbdd y despliegue Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,49 @@
-// JavaScript Document
-// GET /quizes/question
-exports.question = function(req, res) {
-   res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {   
+  models.Quiz.findById(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
+        
+// GET /quizes
+exports.index = function(req, res) {
+console.log('busca todas las preguntas');
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index', { quizes: quizes});
+    }
+  ).catch(function(error) { next(error);})
 };
 
-// GET /quizes/answer
+//GET /quizes
+exports.index = function(req,res){
+      var search = req.query['search'] || "" ;
+      models.Quiz.findAll({where: {pregunta: {like: '%' + search + '%'}}}).then(
+        function(quizes){
+            res.render('quizes/index', {quizes: quizes});
+        }).catch(function(error) {next(error);}); 
+};
+
+    
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-   } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-   }
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
 // GET /author
@@ -18,4 +51,7 @@ exports.author = function(req, res) {
    res.render('author');
 };
 
-
+// GET /buscador
+exports.buscador = function(req, res) {
+   res.render('buscador');
+};
